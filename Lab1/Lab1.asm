bdfe9282cabb8ca90ed509687a8db129e14d8170
setsunayyw <yangyiwen.sigo@hotmail.com>
2021-09-20 17:09:27 +0800

    Add comments on Lab 1

--- Lab1/Lab1.asm
+++ Lab1/Lab1.asm
@@ -3,9 +3,15 @@ str: .string "Hello world!"
 
 .text
     la a1, str
+
+    # Set the address in x22 as 0x10000100
     lui x22, 0x10000
     addi x22, x22, 0x100
+
+    # Store str in x22
     sw a1, 48(x22)
+
+    # Print str
     lw a0, 48(x22)
     li a7, 4
     ecall
