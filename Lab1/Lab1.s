96bdfc5f841522a77a241d09be8e0be730976376
setsunayyw <yangyiwen.sigo@hotmail.com>
2021-09-23 15:56:25 +0800

    Update

--- Lab1/Lab1.s
+++ Lab1/Lab1.s
@@ -2,36 +2,36 @@
 str: .string "Hello world!"
 
 .text
-    lui a1, 0x10000
+    lui s0, 0x10000
 
     # Set target address
-    lui s0, 0x10000
-    addi s0, s0, 0x100
+    lui s3, 0x10000
+    addi s3, s3, 0x100
 
     # Length of string
-    addi a2, a2, 12
+    addi s1, s1, 12
 
     # Store str to the new address
 Loop:
-    add s2, a3, a1    # original address
-    lb t0, 0(s2)    # load one char from original str
-    add s2, a3, s0    # target address
-    sb t0, 0(s2)    # save to target address
-    beq a3, a2, EndLoop    # reach end of str
-    addi a3, a3, 1    # next char
+    add s4, s2, s0    # original address
+    lb t0, 0(s4)    # load one char from original str
+    add s4, s2, s3    # target address
+    sb t0, 0(s4)    # save to target address
+    beq s2, s1, EndLoop    # reach end of str
+    addi s2, s2, 1    # next char
     beq x0, x0, Loop    # loop
 EndLoop:
-    add a3, x0, x0    # reset the counter to 0
+    add s2, x0, x0    # reset the counter to 0
 
     # Print str in the new address
 
     addi a7, a7, 11    # set system call to char mode
 Print:
-    add s2, s0, a3
-    lb a0, 0(s2)
+    add s4, s3, s2
+    lb a0, 0(s4)
     ecall
-    beq a3, a2, Exit
-    addi a3, a3, 1
+    beq s2, s1, Exit
+    addi s2, s2, 1
     beq x0, x0, Print
     
 Exit:
