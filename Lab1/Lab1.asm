4e72fd4f800f8dbfa0f355c43ef7aaf5a971951a
setsunayyw <yangyiwen.sigo@hotmail.com>
2021-09-23 13:25:21 +0800

    Update

--- Lab1/Lab1.asm
+++ Lab1/Lab1.asm
@@ -3,15 +3,38 @@ str: .string "Hello world!"
 
 .text
     la a1, str
+    # lui a1, 0x10000
+    # lw a2, 48(a1)
 
     # Set the address in x22 as 0x10000100
-    lui x22, 0x10000
-    addi x22, x22, 0x100
+    lui s0, 0x10000
+    addi s0, s0, 0x100
+
+    addi a2, a2, 12
+    # sw a1, 48(s0)
 
     # Store str in x22
-    sw a1, 48(x22)
+Loop:
+    add s2, a3, a1
+    add s3, a3, s0
+    lw t0, 0(s2)
+    sw t0, 0(s3)
+    beq a3, a2, EndLoop
+    addi a3, a3, 1
+    beq x0, x0, Loop
+EndLoop:
+    li a3, 0
 
     # Print str
-    lw a0, 48(x22)
-    li a7, 4
+
+    li a7, 11
+Print:
+    add s2, s0, a3
+    lw a0, 0(s2)
     ecall
+    beq a3, a2, Exit
+    addi a3, a3, 1
+    beq x0, x0, Print
+    
+Exit:
+    nop
