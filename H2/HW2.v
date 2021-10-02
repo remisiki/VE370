35718fb027f68717eb36885f87b822ae8e6c5cb6
yangyiwen <yangyiwen.sigo@hotmail.com>
2021-10-02 18:02:26 +0800

    Complete HW2

--- H2/HW2.v
+++ H2/HW2.v
@@ -52,3 +52,33 @@ module Reg32bit (
         end
     end
 endmodule : Reg32bit
+
+module ImmGen32bit (
+    input [31:0] instruction,
+    output reg [31:0] immediate
+
+);
+    always @(*)
+        case (instruction[6:0])
+            // R-type
+            7'b0110011: assign immediate = 'bz;
+            // I-type
+            7'b0000011: assign immediate = {{20{instruction[31]}}, instruction[31:20]}; 
+            7'b0001111: assign immediate = {{20{instruction[31]}}, instruction[31:20]};
+            7'b0010011: assign immediate = {{20{instruction[31]}}, instruction[31:20]};
+            7'b1100111: assign immediate = {{20{instruction[31]}}, instruction[31:20]};
+            7'b1110011: assign immediate = {{20{instruction[31]}}, instruction[31:20]};
+            // S-type
+            7'b0100011: assign immediate = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+            // B-type
+            7'b1100011: assign immediate = {{20{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
+            // U-type
+            7'b0010111: assign immediate = {{12{instruction[31]}}, instruction[31:12]};
+            7'b0110111: assign immediate = {{12{instruction[31]}}, instruction[31:12]};
+            // J-type
+            7'b1101111: assign immediate = {{12{instruction[31]}}, instruction[31], instruction[19:12], 
+                instruction[20], 
+                instruction[30:21]};
+            default: assign immediate = 'bz;
+        endcase
+endmodule : ImmGen32bit
--- H2/sim.v
+++ H2/sim.v
@@ -23,46 +23,57 @@
 module sim(
 
     );
-    wire [31:0] read_data_1, read_data_2;
-    reg [4:0] read_addr_1, read_addr_2, write_addr;
-    reg [31:0] write_data;
-    reg clk, read_en_1, read_en_2, write_en;
-    Reg32bit uut (clk, read_en_1, read_en_2, write_en, read_addr_1, read_addr_2, write_addr, write_data, read_data_1, read_data_2);
+    // wire [31:0] read_data_1, read_data_2;
+    // reg [4:0] read_addr_1, read_addr_2, write_addr;
+    // reg [31:0] write_data;
+    // reg clk, read_en_1, read_en_2, write_en;
+    // Reg32bit uut (clk, read_en_1, read_en_2, write_en, read_addr_1, read_addr_2, write_addr, write_data, read_data_1, read_data_2);
+    reg [31:0] instruction;
+    wire [31:0] immediate;
+    ImmGen32bit uut1 (instruction, immediate);
     initial begin
-        #0  clk = 0;
-            read_addr_1 = 5'b10010;
-            read_addr_2 = 5'b00111;
-            write_data = 32'd3108;
-            read_en_1 = 0;
-            read_en_2 = 0;
-            write_en = 0;
-            write_addr = 5'b10010;
-        #50 write_en = 1;
-        #50 write_en = 0;
-            read_en_1 = 1;
-        #50 read_en_1 = 0;
-            write_en = 1;
-            write_addr = 5'b00111;
-            write_data = 32'd191;
-        #50 write_en = 0;
-        #50 read_en_1 = 1;
-            read_en_2 = 1;
-        #50 read_en_1 = 0;
-            read_en_2 = 0;
-            write_en = 1;
-            write_data = 32'hffffffff;
-        #50 write_en = 0;
-            read_en_2 = 1;
-        #50 read_en_2 = 0;
-            read_addr_1 = 5'b01011;
-            write_addr = 5'b01011;
-            write_data = 32'b0;
-        #50 write_en = 1;
-        #50 write_en = 0;
-            read_en_1 = 1;
-        #50 read_en_1 = 0;
+        // #0  clk = 0;
+        #0 instruction = 32'b00000000110000000000100100110011; // add x18, x0, x12
+        #50 instruction = 32'b11111110000000010010101010000011; // lw x21, -32(sp)
+        #50 instruction = 32'b00000000000100010010010000100011; // sw x1, 8(sp)
+        #50 instruction = 32'b00000000010100000100010001100011; // blt x0, x5, 8
+        #50 instruction = 32'b00010000000000000000010100110111; // lui x10, 0x10000
+        #50 instruction = 32'b11111111000111111111000001101111; // jal x0, -16
+        //     read_addr_1 = 5'b10010;
+        //     read_addr_2 = 5'b00111;
+        //     write_data = 32'd3108;
+        //     read_en_1 = 0;
+        //     read_en_2 = 0;
+        //     write_en = 0;
+        //     write_addr = 5'b10010;
+        // #50 write_en = 1;
+        // #50 write_en = 0;
+        //     read_en_1 = 1;
+        // #50 read_en_1 = 0;
+        //     write_en = 1;
+        //     write_addr = 5'b00111;
+        //     write_data = 32'd191;
+        // #50 write_en = 0;
+        // #50 read_en_1 = 1;
+        //     read_en_2 = 1;
+        // #50 read_en_1 = 0;
+        //     read_en_2 = 0;
+        //     write_en = 1;
+        //     write_data = 32'hffffffff;
+        // #50 write_en = 0;
+        //     read_en_2 = 1;
+        // #50 read_en_2 = 0;
+        //     read_addr_1 = 5'b01011;
+        //     write_addr = 5'b01011;
+        //     write_data = 32'b0;
+        // #50 write_en = 1;
+        // #50 write_en = 0;
+        //     read_en_1 = 1;
+        // #50 read_en_1 = 0;
     end
-    initial begin
-        forever #10 clk = ~ clk;
-    end
+    // initial begin
+    //     forever #10 clk = ~ clk;
+    // end
+    initial
+        #300 $stop;
 endmodule
